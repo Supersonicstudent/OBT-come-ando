1cad833ce397a2e8e2db8cd02a0d60b4e9527103
Supersonicstudent <kalelgermano@gmail.com>
2024-06-21 20:22:45 -0300

    tamo indo...

--- fetchandpreprocessing.py
+++ fetchandpreprocessing.py
@@ -1,26 +1,20 @@
-from gmaps import maps
-from pprint import pprint
-import googlemaps 
-import requests
-from PIL import Image
-from io import BytesIO
+import ee
+# Initialize Earth Engine
+ee.Initialize()
+# Define the date range
+start_date = '2020-01-01'
+end_date = '2020-12-31'
 
-def obter_imagem_mapa(api_key, location='São Paulo, Brazil', zoom=15, tamanho='600x400'):
-    url = 'https://maps.googleapis.com/maps/api/staticmap'
-    params = {
-        'center': location,
-        'zoom': zoom,
-        'size': tamanho,
-        'key': api_key
-    }
-    response = requests.get(url, params=params)
-    image = Image.open(BytesIO(response.content))
-    return image
+# Select Landsat 8 imagery
+image_collection = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
 
-# Exemplo de utilização
-api_key = 'AIzaSyDdTREWbb7NJRvkBjReLpRdgNIyqJeLcbM'
-imagem_mapa = obter_imagem_mapa(api_key)
-imagem_mapa.show()
+# Filter the image collection by date
+image = image_collection.filterDate(start_date, end_date).first()
+# Create a Map object
+Map = ee.Map(zoom=6, center=(40, -120))
+
+# Add the Landsat 8 image to the map
+Map.addLayer(image, {'bands': ['B5', 'B4', 'B3']}, 'Landsat 8')
 
 
 
