87a49936cfedbdce0d9cf013017148fde269002c
Supersonicstudent <kalelgermano@gmail.com>
2024-06-22 18:29:47 -0300

    primeiro filtro teste sendo realizado

--- fetchandpreprocessing.py
+++ fetchandpreprocessing.py
@@ -1,20 +1,66 @@
-import ee
-# Initialize Earth Engine
-ee.Initialize()
-# Define the date range
-start_date = '2020-01-01'
-end_date = '2020-12-31'
+from pprint import pprint
+import googlemaps 
+import requests
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
+import pandas as pd 
+import json
 
-# Select Landsat 8 imagery
-image_collection = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
+def obter_imagem_mapa(api_key, location, zoom, width, height):
+    url = 'https://maps.googleapis.com/maps/api/staticmap'
+    params = {
+        'center': location,
+        'zoom': zoom,
+        'size': f"{width}x{height}",
+        'key': api_key
+    }
+    response = requests.get(url, params=params)
+    image = Image.open(BytesIO(response.content))
+    return image
 
-# Filter the image collection by date
-image = image_collection.filterDate(start_date, end_date).first()
-# Create a Map object
-Map = ee.Map(zoom=6, center=(40, -120))
+# Exemplo de utilização
+api_key = 'AIzaSyDdTREWbb7NJRvkBjReLpRdgNIyqJeLcbM'
+location = 'Quadra E, Rua A, Valparaíso de Goiás, Goiás, Brazil'
+zoom = 20 # Tente um nível de zoom mais baixo para uma área maior
+imagem_mapa = obter_imagem_mapa(api_key, location, zoom, width=2048, height=1366)
+imagem_mapa.show()
 
-# Add the Landsat 8 image to the map
-Map.addLayer(image, {'bands': ['B5', 'B4', 'B3']}, 'Landsat 8')
+gmaps = googlemaps.Client(key=api_key)
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via driving
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="driving",
+                                     departure_time=now)
+# Convert the result of the directions request to JSON
+json_data = json.dumps(directions_result)
+data = json.loads(json_data)
+
+# Access the 'legs' list where distance information is stored
+legs = data[0]['legs']  # Corrigido para acessar o primeiro item da lista de direções
+
+# Create an empty list to store distances
+distances = []
+
+# Loop through each leg and extract the distance information
+for leg in legs:
+    distance_dict = leg['distance']
+    distance_text = distance_dict['text']
+    distances.append(distance_text)
+
+# Create a pandas Series from the list of distances
+distance_series = pd.Series(distances)
+
+# Print the Series containing just the distances
+print(distance_series)
 
 
 
