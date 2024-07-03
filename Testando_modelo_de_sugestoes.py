8f399bf4411d802480ea5fe6867fced24bf3fa82
Supersonicstudent <kalelgermano@gmail.com>
2024-07-03 19:16:00 -0300

    .

--- Testando_modelo_de_sugestoes.py
+++ Testando_modelo_de_sugestoes.py
@@ -2,11 +2,13 @@ from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 
 def train_recommendation_model():
-    # Supondo que você tenha um DataFrame 'traffic_data' com informações históricas
-    # traffic_data deve conter colunas como 'congestion_level', 'improvement_type', 'success_rate', etc.
-    traffic_data = pd.read_csv('historical_traffic_data.csv')
+    # Utilizando o DataFrame fictício
+    traffic_data = pd.DataFrame(data)
     
-    features = traffic_data[['congestion_level', 'road_type', 'time_of_day']]
+    # Conversão de variáveis categóricas para numéricas
+    traffic_data = pd.get_dummies(traffic_data, columns=['road_type', 'time_of_day'])
+    
+    features = traffic_data.drop('improvement_type', axis=1)
     labels = traffic_data['improvement_type']
     
     model = RandomForestClassifier()
@@ -14,11 +16,11 @@ def train_recommendation_model():
     
     return model
 
-def suggest_improvements(congestion_coords, congestion_level, model):
+def suggest_improvements(congestion_coords, congestion_level, road_type, time_of_day, model):
     improvements = []
     
     for coord in congestion_coords:
-        feature_vector = [congestion_level, 'road_type_example', 'time_of_day_example']
+        feature_vector = [congestion_level] + list(pd.get_dummies(pd.Series({'road_type': road_type, 'time_of_day': time_of_day})).iloc[0])
         improvement_suggestion = model.predict([feature_vector])
         
         improvements.append({
@@ -31,7 +33,7 @@ def suggest_improvements(congestion_coords, congestion_level, model):
 # Exemplo de uso
 recommendation_model = train_recommendation_model()
 congestion_coords = np.argwhere(segmentada > 0.6)
-improvements = suggest_improvements(congestion_coords, congestion_level=0.8, model=recommendation_model)
+improvements = suggest_improvements(congestion_coords, congestion_level=0.8, road_type='highway', time_of_day='morning', model=recommendation_model)
 
 result = {
     'start_address': start_address,
